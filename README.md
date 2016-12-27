7378d4d6b0bc109841547eed9a65686cbf0176fd
Brian Connelly <bdc@bconnelly.net>
2016-12-27 09:51:00 -0800

    Added Figure 4

--- README.md
+++ README.md
@@ -21,7 +21,7 @@ These subdirectories may contain additional information:
 | 1       | N/A            | N/A           | [figures/avida_diagram.png](figures/avida_diagram.png) |
 | 2       | (a) [data/cooperators.csv.bz2](data/cooperators.csv.bz2), (b) [data/eps.csv.bz2](data/eps.csv.bz2), (c) [data/kills.csv.bz2](data/kills.csv.bz2), (d) [data/popsizes.csv.bz2](data/popsizes.csv.bz2), [data/popsizes_noEPS.csv.bz2](data/popsizes_noEPS.csv.bz2) | [analysis/plot_avida_combined.R](analysis/plot_avida_combined.R) | [figures/avida_combined.pdf](figures/avida_combined.pdf) |
 | 3       | (a) [data/coop_cheats_r010_411.csv](data/coop_cheats_r010_411.csv), (b) [data/coop_cheats_r030_411.csv](data/coop_cheats_r030_411.csv) | [analysis/plot_avida_popstacks.R](analysis/plot_avida_popstacks.R) | [figures/avida_population_stacks.pdf](figures/avida_population_stacks.pdf) |
-| 4       | TODO           | TODO          | [figures/avida_fluctuation_cooppct.pdf](figures/avida_fluctuation_cooppct.pdf) |
+| 4       | [data/fluctuation_treatments.csv](data/fluctuation_treatments.csv), [data/fluctuation_cooppct.csv](data/fluctuation_cooppct.csv) | [analysis/plot_avida_fluctuation.R](analysis/plot_avida_fluctuation.R) | [figures/avida_fluctuation_cooppct.pdf](figures/avida_fluctuation_cooppct.pdf) |
 | 5       | TODO           | TODO          | [figures/vc_biofilm_7dWT.pdf](figures/vc_biofilm_7dWT.pdf) |
 | 6       | TODO           | TODO          | [figures/TODO.pdf](figures/TODO.pdf) |
 | 7       | TODO           | TODO          | [figures/TODO.pdf](figures/TODO.pdf) |
---
+++ analysis/plot_avida_fluctuation.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+
+source("analysis/common.R")
+
+# Transitions at 30k and 60k
+treatments <- read_csv(file = "data/fluctuation_treatments.csv",
+                       col_types = list(Treatment = col_factor(levels = c("10-40-10", "18-20-18", "20-18-20", "40-10-40")),
+                                        Resource = col_factor(levels = c(10, 18, 20, 40)))) %>%
+    mutate(Update = 0, Frac_Cooperators = 0) %>%
+    filter(Treatment %in% c("10-40-10", "40-10-40"))
+
+d <- read_csv(file = "data/fluctuation_cooppct.csv",
+              col_types = list(Treatment = col_factor(levels = c("10-40-10", "18-20-18", "20-18-20", "40-10-40")),
+                               Replicate = col_factor(levels = 0:50))) %>%
+    mutate(Frac_Cooperators = Cooperators / Organisms) %>%
+    filter(Treatment %in% c("10-40-10", "40-10-40"))
+
+
+pFCoopPct <- ggplot(data = d, aes(x = Update / 1000, y = Frac_Cooperators)) +
+    facet_grid(Treatment ~ .) +
+    geom_rect(data = treatments,
+              aes(xmin = Start / 1000, xmax = End / 1000, ymin = 0, ymax = 1,
+                  fill = Resource),
+              color = NA, alpha=0.5) +
+    stat_summary(fun.data = "mean_cl_boot", geom = "ribbon", color = NA,
+                 alpha = 0.2) +
+    stat_summary(fun.y = "mean", geom = "line") +
+    geom_text(data = treatmentsX,
+              aes(label = Resource, x = (Start + ((End - Start) / 2)) / 1000),
+              y = 0.97, hjust = 0.5, vjust = 1, size = 3, fontface = "italic") +
+    scale_y_continuous(limits=c(0,1)) +
+    scale_fill_manual(values = c("10" = "grey90", "40" = "grey60"),
+                      guide = FALSE) +
+    labs(x = label_time, y = label_cooppct) + 
+    theme(strip.text = element_blank())
+
+ggsave_golden(filename = "figures/avida_fluctuation_cooppct.pdf",
+              plot = pFCoopPct)
