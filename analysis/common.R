407c9f9b1290406ba7e6f2090891777176699ddf
Brian Connelly <bdc@bconnelly.net>
2017-01-05 10:33:16 -0800

    Removed theme_rescoop
    - theme_rescoop was just a copy of ggplot2bdc::theme_bdc_grey, and since
    ggplot2bdc is being used anyway, removed this duplication

--- analysis/common.R
+++ analysis/common.R
@@ -4,8 +4,7 @@ library(ggplot2bdc)
 
 source("analysis/formatting.R")
 
-source("analysis/theme_rescoop.R")
-theme_set(theme_rescoop(base_size = 17, pmargin = 20))
+theme_set(theme_bdc_grey(base_size = 17, pmargin = 20))
 
 get_last_update <- . %>% filter(Update == max(.$Update))
 
